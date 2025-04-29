8a718f0f1d18c311d927fb61bb1caf6ba2be6327
zys <zyszys1999@163.com>
2025-04-29 15:24:44 +0800

    solution for backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,13 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace(void)  {
+  uint64 fp = r_fp();
+  printf("backtrace:\n");
+  while(PGROUNDDOWN(fp) != PGROUNDUP(fp)) {
+    uint64 ra = *(uint64*)(fp - 8);
+    printf("%p\n", ra);
+    fp = *(uint64*)(fp - 16);
+  }
+}
