5786516bfea95afe0a4c9537f7b4e29c4bb98229
zys <zyszys1999@163.com>
2025-04-29 17:05:55 +0800

    solution for sbrk

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,17 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  // if(growproc(n) < 0)
+  //   return -1;
+  
+  struct proc* p = myproc();
+  if(n > 0) {
+    p->sz += n;
+  } else if (p->sz + n > 0) {
+    p->sz = uvmdealloc(p->pagetable, p->sz, p->sz + n);
+  } else {
     return -1;
+  }
   return addr;
 }
 
